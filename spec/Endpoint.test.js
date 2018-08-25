ff7ae3eb3aa290a6c51a66d3abd8178be5923a4c
Florian Keller <github@floriankeller.de>
2018-08-25 13:51:33 +0200

    Add Endpoint test

---
+++ spec/Endpoint.test.js
@@ -0,0 +1,13 @@
+//@ts-check
+
+const {Endpoint} = require('../dist/Endpoint');
+
+describe('Endpoint', () => {
+  it('concats platform and name', () => {
+    const platform = 'npm';
+    const name = 'grunt';
+
+    const endpoint = Endpoint.Project.contributors(platform, name);
+    expect(endpoint).toBe('/npm/grunt/contributors');
+  })
+})
